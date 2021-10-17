beb04d67eca99e8b6b154e9be3b01240e0d99af1
1core2life <whitezonen1@gmail.com>
2021-10-17 20:00:13 +0900

    structure

--- server.js
+++ server.js
@@ -1,157 +1,34 @@
 
-var express = require('express');
-var app = express();
 var path = require('path');
 var fs = require("fs")
-var mysql = require('mysql');
-var pool = mysql.createPool({
-    connectionLimit : 10,
-    user : 'root',
-    password : '',
-    database: 'global'
-})
+var bodyParser = require("body-parser");
+
+var express = require('express');
+var app = express();
 
 var server = app.listen( process.env.PORT || 3000, function(){
  console.log("Express server has started on port " + process.env.PORT);
- console.log("enter IP: " + process.env.IP);
 });
  
-var bodyParser = require("body-parser");
-var fs = require("fs");
  
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
-app.set('views', path.join(__dirname, 'views'));
 
 
 var router = require('./main')(app);
 
 
-app.get('/global', function(req, res){
-
-    req, res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'}); // header 설정
-    fs.readFile(__dirname + '/static/templates/index.html', (err, data) => { // 파일 읽는 메소드
-        if (err) 
-            return console.error(err); // 에러 발생시 에러 기록하고 종료
-    
-        req, res.end(data, 'utf-8'); // 브라우저로 전송
-    });
-
-
-});
-    
-    
-    
-app.get('/global/posting', function(req, res){
-
-    req, res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'}); // header 설정
-    fs.readFile(__dirname + '/static/templates/posting.html', (err, data) => { // 파일 읽는 메소드
-        if (err) 
-            return console.error(err); // 에러 발생시 에러 기록하고 종료
-    
-        req, res.end(data, 'utf-8'); // 브라우저로 전송
-    });
-
-
-});
-
-    
-    
-app.get('/global/card', function(req, res){
-
-    req, res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'}); // header 설정
-    fs.readFile(__dirname + '/static/templates/card.html', (err, data) => { // 파일 읽는 메소드
-        if (err) 
-            return console.error(err); // 에러 발생시 에러 기록하고 종료
-    
-        req, res.end(data, 'utf-8'); // 브라우저로 전송
-    });
-
-
-});
-
-app.get('/global/morePost', function(req, res){
-
-
-      pool.getConnection(function(err,con){
-       
-          con.query('select * from tb_post order by hit desc limit 1 offset 1',function(err,result){
-            if (err) {
-                console.error(err);
-                throw err;
-            }
-
-            res.send(200,result[0]);
-
-
-          });
-      });
-
-
-});
-
-
-
-app.post('/global/newPost', function(req, res){
-
-    var subject = req.body.subject
-    var location = req.body.location
-    var userIp = req.body.userIp
-    var content =req.body.content
-
-
-
-     var now = new Date();
-      
-      var year = now.getFullYear();
-      var month = (now.getMonth()+1);
-      var day = now.getDate();
-      var hour = now.getHours();
-      var min = now.getMinutes();
-      var sec = now.getSeconds();
-      var mill = now.getMilliseconds();
-      
-      var nowTime = year +'-'+ month+'-'+ day+' '+ hour+':'+min+':'+ sec;
-
-      pool.getConnection(function(err,con){
-          var cont = [
-              0,
-              location,
-              '0',
-              subject,
-              content,
-              nowTime,
-              userIp,
-              0
-          ]
-          
-          con.query('insert into tb_post values(?,?,?,?,?,?,?,?)',cont,function(err,result){
-            if (err) {
-                console.error(err);
-                throw err;
-            }
-
-            console.log(result);
-            res.send(200,'success');
-
-
-          });
-      });
-
-
-});
-
 
 app.get('/mapGl', function(req, res){
-    var img = fs.readFileSync('./public/mapGl.jpg');
+    var img = fs.readFileSync('./static/img/mapGl.jpg');
     res.writeHead(200, {'Content-Type': 'image/jpg' });
     res.end(img, 'binary');
 
 });
 
 app.get('/mapSeol', function(req, res){
-    var img = fs.readFileSync('./public/mapSeol.jpg');
+    var img = fs.readFileSync('./static/img/mapSeol.jpg');
     res.writeHead(200, {'Content-Type': 'image/jpg' });
     res.end(img, 'binary');
 
